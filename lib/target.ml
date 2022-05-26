05cc477410fdc9ea772971a35edd0849c0afed78
xvw <xavier.vdw@gmail.com>
2022-05-26 08:12:32 +0200

    Relativize to `capsule`

--- lib/target.ml
+++ lib/target.ml
@@ -1,11 +1,12 @@
 open Yocaml
 
+let capsule ~target = "capsule" |> into target
 let md_to_html file = basename $ replace_extension file "html"
-let css ~target = "css" |> into target
-let javascript ~target = "js" |> into target
-let fonts ~target = "fonts" |> into target
-let images ~target = "images" |> into target
-let pages ~target = "pages" |> into target
-let indexes ~target = target
+let css ~target = "css" |> into (capsule ~target)
+let javascript ~target = "js" |> into (capsule ~target)
+let fonts ~target = "fonts" |> into (capsule ~target)
+let images ~target = "images" |> into (capsule ~target)
+let pages ~target = "pages" |> into (capsule ~target)
+let indexes ~target = capsule ~target
 let for_page ~target file = md_to_html file |> into (pages ~target)
 let for_index ~target file = md_to_html file |> into (indexes ~target)
