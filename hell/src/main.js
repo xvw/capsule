be17c21bdfe591a7624a07eddd91cba707cedddc
xvw <xavier.vdw@gmail.com>
2022-12-15 19:12:27 +0100

    Hell: Fix webpack in order to deal with browserify

--- hell/src/main.js
+++ hell/src/main.js
@@ -1,7 +1,11 @@
+import { DAppClient } from "@airgap/beacon-sdk";
+
 import hljs from 'highlight.js/lib/core';
 import ocaml from 'highlight.js/lib/languages/ocaml';
 import java from 'highlight.js/lib/languages/java';
 import kotlin from 'highlight.js/lib/languages/kotlin';
+
+window.DAppClient = DAppClient;
 
 hljs.registerLanguage('ocaml', ocaml);
 hljs.registerLanguage('java', java);
--- hell/webpack.config.js
+++ hell/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/main.js",
@@ -7,5 +8,22 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '_build'),
         filename: 'hell.js'
-    }
+    },
+    resolve: {
+        fallback: {
+            "buffer": require.resolve("buffer"),
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "assert": require.resolve("assert"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "os": require.resolve("os-browserify"),
+            "url": require.resolve("url"),
+            "path": require.resolve("path-browserify") 
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] }),
+        new webpack.ProvidePlugin({ Buffer: 'process/browser' }),
+    ]
 };
