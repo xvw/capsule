396a03ea78ac3c3a449f80ac46521c468fc4d393
xvw <xavier.vdw@gmail.com>
2023-10-24 23:03:23 +0200

    Add Haskell lang

--- hell/src/main.js
+++ hell/src/main.js
@@ -1,13 +1,15 @@
 import { DAppClient } from "@airgap/beacon-sdk";
 
-import hljs from 'highlight.js/lib/core';
-import ocaml from 'highlight.js/lib/languages/ocaml';
-import java from 'highlight.js/lib/languages/java';
-import kotlin from 'highlight.js/lib/languages/kotlin';
+import hljs from "highlight.js/lib/core";
+import ocaml from "highlight.js/lib/languages/ocaml";
+import java from "highlight.js/lib/languages/java";
+import kotlin from "highlight.js/lib/languages/kotlin";
+import haskell from "highlight.js/lib/languages/haskell";
 
 window.DAppClient = DAppClient;
 
-hljs.registerLanguage('ocaml', ocaml);
-hljs.registerLanguage('java', java);
-hljs.registerLanguage('kotlin', kotlin)
+hljs.registerLanguage("ocaml", ocaml);
+hljs.registerLanguage("java", java);
+hljs.registerLanguage("kotlin", kotlin);
+hljs.registerLanguage("haskell", haskell);
 window.hljs = hljs;
