a18c6e1d2d38d5749981e5d98ed5c130f5a71154
xvw <xavier.vdw@gmail.com>
2023-11-09 04:41:39 +0100

    Restore HLJS and remove profil

--- bin/capsule_client/capsule_client.ml
+++ bin/capsule_client/capsule_client.ml
@@ -1,6 +1,15 @@
 open Js_of_ocaml
 open Nightmare_js
 
+module Hljs = struct
+  let get () = Js.Unsafe.global##.hljs
+
+  let mount () =
+    let hljs = get () in
+    let () = hljs##highlightAll in
+    Lwt.return ()
+end
+
 let () = Suspension.allow ()
 
 let () =
@@ -9,4 +18,10 @@ let () =
        method mountTransfer container_id =
          let id = Js.to_string container_id in
          Dapp_transfer.mount id
+    end)
+
+let () =
+  Js.export "capsule_hljs"
+    (object%js
+       method run = Hljs.mount ()
     end)
