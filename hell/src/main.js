a1b5ebd86f0cff5f458d7320c36202dfb1500d19
xvw <xavier.vdw@gmail.com>
2024-09-25 19:28:08 +0200

    Add elisp for HL language

--- hell/src/main.js
+++ hell/src/main.js
@@ -4,11 +4,15 @@ import java from "highlight.js/lib/languages/java";
 import kotlin from "highlight.js/lib/languages/kotlin";
 import haskell from "highlight.js/lib/languages/haskell";
 import diff from "highlight.js/lib/languages/diff";
+import lisp from "highlight.js/lib/languages/lisp";
+import scheme from "highlight.js/lib/languages/scheme";
 
 hljs.registerLanguage("ocaml", ocaml);
 hljs.registerLanguage("java", java);
 hljs.registerLanguage("kotlin", kotlin);
 hljs.registerLanguage("haskell", haskell);
 hljs.registerLanguage("diff", diff);
+hljs.registerLanguage("lisp", diff);
+hljs.registerLanguage("scheme", diff);
 
 window.hljs = hljs;
