be0105310134d9fa376584afab2577a85d1f28ca
xvw <xavier.vdw@gmail.com>
2024-10-11 21:39:17 +0200

    Introduce Generic Key Values

---
+++ lib/static/model/key_value.mli
@@ -0,0 +1,8 @@
+module Make (K : Types.MODEL) (V : Types.MODEL) :
+  Types.KEY_VALUE with type key = K.t and type value = V.t
+
+module By_string (V : Types.MODEL) :
+  Types.KEY_VALUE with type key = string and type value = V.t
+
+module String : Types.KEY_VALUE with type key = string and type value = string
+module Links : Types.KEY_VALUE with type key = string and type value = Link.t
