a9732aeec90461f4e28c11990c021aef5a02554c
xvw <xavier.vdw@gmail.com>
2024-10-17 17:52:08 +0200

    Some tweak on UI and handle JavaScript (through JSOO)

---
+++ hell/README.md
@@ -0,0 +1,4 @@
+# Hell
+
+Compose with third-party libraries in the JavaScript
+ecosystem... hell!
--- lib/generator/file.mli
+++ lib/generator/file.mli
@@ -1,5 +1,6 @@
 val is_markdown : Yocaml.Path.t -> bool
 val is_css : Yocaml.Path.t -> bool
+val is_javascript : Yocaml.Path.t -> bool
 val is_image : Yocaml.Path.t -> bool
 val is_font : Yocaml.Path.t -> bool
 val is_related_to_favicon : Yocaml.Path.t -> bool
