0727f1ece92bdb91e06b06f69278a6806aef6dd2
xvw <xavier.vdw@gmail.com>
2024-11-19 01:57:47 +0100

    A new purge for a new hope

--- bin/widgets/comments/util.mli
+++
@@ -1,7 +0,0 @@
-type node_content =
-  | Link of string * string
-  | Text of string
-  | Paragraph of node_content list
-
-val cheat_with_string_document : string -> string
-val parse_mastodon_response : string -> node_content list
--- lib/dapps/util.mli
+++
@@ -1,2 +0,0 @@
-val truncate_hash : string -> string
-val block_hash_to_string : Yourbones.Block_hash.t -> string
