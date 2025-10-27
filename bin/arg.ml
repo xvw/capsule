5f59d33c109f96a8125307ce36cb30b9ef8cb187
xvw <xavier.vdw@gmail.com>
2025-10-27 15:10:45 +0100

    Some logs and grooming

--- bin/arg.ml
+++ bin/arg.ml
@@ -1,21 +1,24 @@
 let docs = Cmdliner.Manpage.s_common_options
+let yocaml_path = Conv.path
+let port = Conv.port
+let log_level = Conv.log_level
 
 let target ?(default = Yocaml.Path.rel [ "_www" ]) () =
   let doc = "Target directory" in
   let arg = Cmdliner.Arg.info ~doc ~docs [ "target"; "output" ] in
-  Cmdliner.Arg.(value @@ opt Conv.path default arg)
+  Cmdliner.Arg.(value @@ opt yocaml_path default arg)
 ;;
 
 let source ?(default = Yocaml.Path.rel []) () =
   let doc = "Source directory" in
   let arg = Cmdliner.Arg.info ~doc ~docs [ "source"; "input" ] in
-  Cmdliner.Arg.(value @@ opt Conv.path default arg)
+  Cmdliner.Arg.(value @@ opt yocaml_path default arg)
 ;;
 
 let port ?(default = 8888) () =
   let doc = "The development server's listening port" in
   let arg = Cmdliner.Arg.info ~doc ~docs [ "port"; "P" ] in
-  Cmdliner.Arg.(value @@ opt Conv.port default arg)
+  Cmdliner.Arg.(value @@ opt port default arg)
 ;;
 
 let config_file ?(default = "capsule.toml") () =
@@ -27,5 +30,5 @@ let config_file ?(default = "capsule.toml") () =
 let log_level ?(default = `Debug) () =
   let doc = "The log-level of the application running" in
   let arg = Cmdliner.Arg.info ~doc ~docs [ "log-level"; "level"; "log" ] in
-  Cmdliner.Arg.(value @@ opt Conv.log_level default arg)
+  Cmdliner.Arg.(value @@ opt log_level default arg)
 ;;
